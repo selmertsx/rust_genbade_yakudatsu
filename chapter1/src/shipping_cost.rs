1878ba957443ad77ab4daf3d2e044d081e410a75
shuhei mroioka <selmertsx@gmail.com>
2022-02-05 12:00:08 +0900

    refacatoring

--- chapter1/src/shipping_cost.rs
+++ chapter1/src/shipping_cost.rs
@@ -4,19 +4,22 @@ pub struct ShippingCost {
 
 impl ShippingCost {
     const MINIMUM_FOR_FREE: u32 = 3000;
-    const COST:u32 = 500;
+    pub const COST:u32 = 500;
+    pub const FREE: u32 = 0;
 
-    pub fn new(base_price: u32) -> ShippingCost {
+    pub fn new(base_price: u32) -> Self {
         ShippingCost {
             base_price
         }
     }
 
-    pub fn amount(&self) -> u32 {
+    // TODO: 
+    // 返す型の部分で ShippingCost::FREE | ShippingCost::COST のどちらかが必ず帰る。という実装をしたい
+    pub fn amount(&self) ->  u32 {
         if self.base_price < ShippingCost::MINIMUM_FOR_FREE {
             return ShippingCost::COST
         }
 
-        self.base_price
+        ShippingCost::FREE
     }
 }
