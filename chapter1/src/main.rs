a990ce8a3cebac5ef617cd05cb0deececb9aae49
shuhei mroioka <selmertsx@gmail.com>
2022-02-05 11:53:05 +0900

    chapter1 done

--- chapter1/src/main.rs
+++ chapter1/src/main.rs
@@ -2,6 +2,9 @@ mod shipping_cost;
 use shipping_cost::ShippingCost;
 
 fn main() {
-    let shipping_cost = ShippingCost::new(300);
-    println!("Hello, world!");
+    let shipping_cost = ShippingCost::new(2900);
+    println!("{}", shipping_cost.amount());
+    
+    let shipping_cost = ShippingCost::new(3100);
+    println!("{}", shipping_cost.amount());
 }
--- chapter1/src/shipping_cost.rs
+++ chapter1/src/shipping_cost.rs
@@ -3,6 +3,9 @@ pub struct ShippingCost {
 }
 
 impl ShippingCost {
+    const MINIMUM_FOR_FREE: u32 = 3000;
+    const COST:u32 = 500;
+
     pub fn new(base_price: u32) -> ShippingCost {
         ShippingCost {
             base_price
@@ -10,8 +13,8 @@ impl ShippingCost {
     }
 
     pub fn amount(&self) -> u32 {
-        if self.base_price < 3000 {
-            return 500
+        if self.base_price < ShippingCost::MINIMUM_FOR_FREE {
+            return ShippingCost::COST
         }
 
         self.base_price
